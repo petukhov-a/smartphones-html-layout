74c5afcf3ba8402d4708753071b15eb6a2028995
Anton <reopik@mail.ru>
2023-10-04 01:48:29 +0300

    made showing filter sidebar with darkening effect and add slide animation

--- src/js/index.js
+++ src/js/index.js
@@ -11,14 +11,33 @@ checkboxes.forEach((element, index) => {
 // Display filters on mobile
 const filter = document.querySelector('.smartphones-filter');
 const filterBtn = document.querySelector('.smartphones-header__mobile-filter-btn');
+const overlay = document.querySelector('.overlay');
 
-filterBtn.addEventListener('click', () => filter.classList.add('active'));
+const toggleFilterShowing = (isShow) => {
+    if (isShow) {
+        filter.classList.add('active');
+        document.body.classList.add('no-scroll');
+        overlay.style.opacity = '1';
+        overlay.style.zIndex = '101';
+    } else {
+        filter.classList.remove('active');
+        document.body.classList.remove('no-scroll');
+        overlay.style.opacity = '0';
+        setTimeout(() => {
+            overlay.style.zIndex = '-1';
+        }, 300);
+    }
+}
+
+filterBtn.addEventListener('click', () => {
+    toggleFilterShowing(true);
+});
 
 document.addEventListener('click', (event) => {
     const isClickInside = filter.contains(event.target);
     const isClickFilterBtn = filterBtn.contains(event.target)
 
     if (!isClickInside && !isClickFilterBtn) {
-        filter.classList.remove('active');
+        toggleFilterShowing(false);
     }
-})
+});
