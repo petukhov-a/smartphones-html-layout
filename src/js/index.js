0f5940bea15e739e24171d0adfd12932f842ba39
Anton <reopik@mail.ru>
2023-10-03 23:51:15 +0300

    made some changes for responsive. Made closing filter sidebar on click outside

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,4 @@
+// Filter item opening
 const checkboxes = document.querySelectorAll('.smartphones-filter-item__heading');
 const dropDownArrow = document.querySelectorAll('.drop-down-arrow');
 
@@ -5,4 +6,19 @@ checkboxes.forEach((element, index) => {
     element.addEventListener('click', () => {
         dropDownArrow[index].classList.toggle('active');
     });
+})
+
+// Display filters on mobile
+const filter = document.querySelector('.smartphones-filter');
+const filterBtn = document.querySelector('.smartphones-header__mobile-filter-btn');
+
+filterBtn.addEventListener('click', () => filter.classList.add('active'));
+
+document.addEventListener('click', (event) => {
+    const isClickInside = filter.contains(event.target);
+    const isClickFilterBtn = filterBtn.contains(event.target)
+
+    if (!isClickInside && !isClickFilterBtn) {
+        filter.classList.remove('active');
+    }
 })
