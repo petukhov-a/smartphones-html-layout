d647ccfe03b21b02cee96f2bc443e1d67ed65084
Anton <reopik@mail.ru>
2023-10-04 23:57:00 +0300

    made some changes for responsive. Made sort menu for mobile.

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,15 @@
+// Common
+const handleOutsideClick = (elemnt, btn, hideElement) => {
+    document.addEventListener('click', (event) => {
+        const isClickInside = elemnt.contains(event.target);
+        const isClickBtn = btn.contains(event.target)
+    
+        if (!isClickInside && !isClickBtn) {
+            hideElement();
+        }
+    });
+}
+
 // Filter item opening
 const checkboxes = document.querySelectorAll('.smartphones-filter-item__heading');
 const dropDownArrow = document.querySelectorAll('.drop-down-arrow');
@@ -6,7 +18,7 @@ checkboxes.forEach((element, index) => {
     element.addEventListener('click', () => {
         dropDownArrow[index].classList.toggle('active');
     });
-})
+});
 
 // Display filters on mobile
 const filter = document.querySelector('.smartphones-filter');
@@ -33,11 +45,16 @@ filterBtn.addEventListener('click', () => {
     toggleFilterShowing(true);
 });
 
-document.addEventListener('click', (event) => {
-    const isClickInside = filter.contains(event.target);
-    const isClickFilterBtn = filterBtn.contains(event.target)
+handleOutsideClick(filter, filterBtn, () => toggleFilterShowing(false));
 
-    if (!isClickInside && !isClickFilterBtn) {
-        toggleFilterShowing(false);
-    }
+// Show sort
+const sortMenu = document.querySelector('.sort-list ul');
+const sortBtn = document.querySelector('.sort-mobile-btn');
+
+sortBtn.addEventListener('click', () => {
+    sortMenu.style.display = 'flex';
+});
+
+handleOutsideClick(sortMenu, sortBtn, () => {
+    sortMenu.style.display = 'none';
 });
